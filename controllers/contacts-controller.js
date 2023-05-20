df2491f3beefd4a323492c1daa626106bace8392
Ivan Malenko <ivan.malenko18@gmail.com>
2023-05-21 00:54:16 +0300

    added getAll and getById

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -1,80 +1,81 @@
-const Joi = require('joi');
+// const Joi = require('joi');
 const { HttpError } = require("../helpers/httpError");
 const { ctrlWrapper } = require('../decorators/ctrlWrapper');
-const { listContacts, getContactById,removeContact, addContact, updateContact } = require('../models/contacts');
-
+// const { getContactById,removeContact, addContact, updateContact } = require('../models/contact');
+const Contacts = require("../models/contact");
 
 
 
 
 
 const fetchAllContacts = async (req, res) => {
-    const contacts = await listContacts();
+    const contacts = await  Contacts.find();
   res.json(contacts);
 
 };
 
 const fetchContactById = async (req, res ) => { 
-    const id = req.params.contactId;
-    const contactById = await getContactById(id);
+  const id = req.params.contactId;
+  
+  const contactById = await Contacts.findById(id);
     if (!contactById) { throw HttpError(404) };
     res.json(contactById);
 
 };
 
-const deleteContact = async (req, res ) => { 
-    const id = req.params.contactId;
-    const deletedContact = await removeContact(id);
-    if (!deletedContact) { throw HttpError(404) }
-    res.json({ message: 'contact deleted' })
+// const deleteContact = async (req, res ) => { 
+//     const id = req.params.contactId;
+//     const deletedContact = await removeContact(id);
+//     if (!deletedContact) { throw HttpError(404) }
+//     res.json({ message: 'contact deleted' })
 
-};
+// };
 
-const addTheContact = async (req, res) => { 
+// const addTheContact = async (req, res) => { 
 
-   const schema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().pattern(/^\d{10}$/).required()
-});
-    const { error } = schema.validate(req.body, { abortEarly: false });
-    if (error) {
-      throw HttpError(400, "Missing required field")
-    }
+//    const schema = Joi.object({
+//   name: Joi.string().required(),
+//   email: Joi.string().email().required(),
+//   phone: Joi.string().pattern(/^\d{10}$/).required()
+// });
+//     const { error } = schema.validate(req.body, { abortEarly: false });
+//     if (error) {
+//       throw HttpError(400, "Missing required field")
+//     }
 
-    const newUser = await addContact(req.body);
-    res.status(201).json(newUser);
+//     const newUser = await addContact(req.body);
+//     res.status(201).json(newUser);
 
  
-};
+// };
 
-const updateTheContact = async (req, res) => { 
-    const schema = Joi.object({
-      name: Joi.string(),
-      email: Joi.string().email(),
-      phone: Joi.string().pattern(/^\d{10}$/)
-    }).or('name', 'email', 'phone').required();
+// const updateTheContact = async (req, res) => { 
+//     const schema = Joi.object({
+//       name: Joi.string(),
+//       email: Joi.string().email(),
+//       phone: Joi.string().pattern(/^\d{10}$/)
+//     }).or('name', 'email', 'phone').required();
 
-    const { error } = schema.validate(req.body, { abortEarly: false });
-    if (error) {
-      throw HttpError(400, "Missing field")
-    }
-    const id = req.params.contactId;
-    const body = req.body;
-    const updatedContact = await updateContact(id, body);
-    if (!updatedContact) { throw HttpError(404) };
-    res.json(updatedContact);
+//     const { error } = schema.validate(req.body, { abortEarly: false });
+//     if (error) {
+//       throw HttpError(400, "Missing field")
+//     }
+//     const id = req.params.contactId;
+//     const body = req.body;
+//     const updatedContact = await updateContact(id, body);
+//     if (!updatedContact) { throw HttpError(404) };
+//     res.json(updatedContact);
 
 
-};
+// };
 
 
 
 module.exports = {
-    fetchAllContacts: ctrlWrapper(fetchAllContacts),
-    fetchContactById: ctrlWrapper(fetchContactById),
-    deleteContact: ctrlWrapper(deleteContact),
-    addTheContact: ctrlWrapper(addTheContact),
-    updateTheContact: ctrlWrapper(updateTheContact),
+  fetchAllContacts: ctrlWrapper(fetchAllContacts),
+  fetchContactById: ctrlWrapper(fetchContactById),
+  // deleteContact: ctrlWrapper(deleteContact),
+  // addTheContact: ctrlWrapper(addTheContact),
+  // updateTheContact: ctrlWrapper(updateTheContact),
 
-};
+}
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express.Router();
-const { fetchAllContacts, fetchContactById, deleteContact,addTheContact,updateTheContact } = require('../../controllers/contacts-controller');
+const { fetchAllContacts,fetchContactById,
+    //  deleteContact, addTheContact, updateTheContact
+} = require('../../controllers/contacts-controller');
 
 
 router.get('/', fetchAllContacts);
 router.get('/:contactId', fetchContactById);
-router.delete('/:contactId', deleteContact);
-router.post('/', addTheContact);
-router.put('/:contactId', updateTheContact);
+// router.delete('/:contactId', deleteContact);
+// router.post('/', addTheContact);
+// router.put('/:contactId', updateTheContact);
 
 module.exports = router;
 
