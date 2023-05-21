29d2c688a970d172f4d11dc7a345e4271bba7d14
Ivan Malenko <ivan.malenko18@gmail.com>
2023-05-21 13:27:55 +0300

    finish

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const router = express.Router();
-const { fetchAllContacts,fetchContactById,
-    //  deleteContact, addTheContact, updateTheContact
+const { fetchAllContacts, fetchContactById, addTheContact,
+    deleteContact,
+    updateFavorite,
+    updateTheContact
 } = require('../../controllers/contacts-controller');
 
 
 router.get('/', fetchAllContacts);
 router.get('/:contactId', fetchContactById);
-// router.delete('/:contactId', deleteContact);
-// router.post('/', addTheContact);
-// router.put('/:contactId', updateTheContact);
+router.delete('/:contactId', deleteContact);
+router.post('/', addTheContact);
+router.put('/:contactId', updateTheContact);
+router.patch("/:contactId/favorite", updateFavorite);
 
 module.exports = router;
 
